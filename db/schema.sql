1bb1faf5a604bb32b214c7cff69a5f03635104d9
Corey96 <0coreyy@gmail.com>
2022-03-21 14:39:13 +0000

    DB set up, first table and show connection to DB

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+drop database if exists employee_db;
+create database employee_db;
+
+use employee_db;
+
+CREATE TABLE employee (
+id int not null,
+employee_name varchar(100) not null
+);
+
--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express')
+const mysql =require('mysql2')
+
 const app = express()
 const port = process.env.PORT || 3001
 
-//test route to prove express server is running//
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'secretpassword',
+    database: 'employee_db'
+});
+
+connection.query('select * from employees', (err, result) => {
+    console.log(result)
+});
+
+
+
+
+
+
+// Test route to prove express server is running //
 app.get('/test', (_req, res) => {
     res.send('hello people')
 })
