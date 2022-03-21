c3fe897bdb9fdf7e32e636c8f536b0f997bd7936
Corey96 <0coreyy@gmail.com>
2022-03-21 13:43:41 +0000

    express setup

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('hello people')
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3001
+
+//test route to prove express server is running//
+app.get('/test', (_req, res) => {
+    res.send('hello people')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+    
+})
+
