dc7438a8ae0ae7f902b6cac96fc761946583a89c
Corey96 <0coreyy@gmail.com>
2022-06-11 17:24:08 +0100

    structure changes and minify files

--- db/connections.js
+++ db/connections.js
@@ -1,4 +1,10 @@
+// modules
 const mysql = require('mysql2');
+const inquirer = require('inquirer')
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 3001
 
 const db = mysql.createConnection(
     {
@@ -6,10 +12,30 @@ const db = mysql.createConnection(
       // MySQL username,
       user: 'root',
       // MySQL password
-      password: 'password123',
+      password: 'secretpassword',
       database: 'employees_db'
     },
     console.log(`Connected to the classlist_db database.`)
   );
+
+  connection.query('select * from employee', (err, result) => {
+    console.log(result)
+});
+
+
+
+
+
+
+// Test route to prove express server is running //
+app.get('/test', (_req, res) => {
+    res.send('hello people')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+    
+})
+
 
   module.exports = db;
