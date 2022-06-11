9238b394b529a71f1a52ca59afd720e8e29c4fe6
Corey96 <0coreyy@gmail.com>
2022-06-11 17:18:45 +0100

    read me update

---
+++ db/connections.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: 'password123',
+      database: 'employees_db'
+    },
+    console.log(`Connected to the classlist_db database.`)
+  );
+
+  module.exports = db;
